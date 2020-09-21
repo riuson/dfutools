1e50910c1a8702492764d4e72a432be784f2fc89
Vladimir <riuson@gmail.com>
2020-09-22 00:17:53 +0500

    Add projects for library and tests

---
+++ Sources/DfusSeConvLib.Tests/Class1.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+namespace DfuSeConvLib.Tests {
+    [TestFixture]
+    public class Class1 {
+        [Test]
+        public void Method1() {
+            Assert.That(true);
+        }
+    }
+}
