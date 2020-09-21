b5e69c03473d9bfee248c11a1e4b1f238f9e240d
Vladimir <riuson@gmail.com>
2020-09-22 03:12:59 +0500

    Add ImageElement

---
+++ Sources/DfusSeConvLib/Extensions/ImageElementExtension.cs
@@ -0,0 +1,19 @@
+﻿using DfuSeConvLib.Parts;
+using System;
+using System.IO;
+using System.Text;
+
+namespace DfuSeConvLib.Extensions {
+    public static class ImageElementExtension {
+        public static uint CalculateSize(this ImageElement imageElement) =>
+            Convert.ToUInt32(4 + 4 + imageElement.Data.Length);
+
+        public static void Write(this ImageElement imageElement, Stream stream) {
+            using (var writer = new BinaryWriter(stream, Encoding.ASCII, true)) {
+                writer.Write(imageElement.ElementAddress);
+                writer.Write(Convert.ToUInt32(imageElement.Data.Length));
+                writer.Write(imageElement.Data);
+            }
+        }
+    }
+}
---
+++ Sources/DfusSeConvLib/Parts/ImageElement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DfuSeConvLib.Parts {
+    /// <summary>
+    /// The Image element structured as follows, provides a data record containing the effective
+    /// firmware data preceded by the data address and data size.The Image Element buffer is
+    /// represented in Big Endian order.
+    /// </summary>
+    public class ImageElement {
+        /// <summary>
+        /// The ElementAddress field gives the 4-byte starting address of the data.
+        /// </summary>
+        public uint ElementAddress { get; set; }
+
+        /// <summary>
+        /// The ElementSize field gives the size of the contained data.
+        /// </summary>
+        public uint ElementSize => Convert.ToUInt32(this.Data.Length);
+
+        /// <summary>
+        /// The Data field present the effective data.
+        /// </summary>
+        public byte[] Data { get; set; }
+    }
+}
