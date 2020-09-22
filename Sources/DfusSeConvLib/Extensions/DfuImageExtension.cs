770fda76a2abaf892819f2be3599c409f6541d40
Vladimir <riuson@gmail.com>
2020-09-22 18:19:47 +0500

    Add Dfuimage

---
+++ Sources/DfusSeConvLib/Extensions/DfuImageExtension.cs
@@ -0,0 +1,19 @@
+﻿using DfuSeConvLib.Parts;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace DfuSeConvLib.Extensions {
+    public static class DfuImageExtension {
+        public static uint CalculateSize(this DfuImage dfuImage) =>
+            dfuImage.Prefix.CalculateSize() + Convert.ToUInt32(dfuImage.ImageElements.Sum(x => x.CalculateSize()));
+
+        public static void Write(this DfuImage dfuImage, Stream stream) {
+            dfuImage.Prefix.Write(stream, dfuImage);
+
+            foreach (var dfuImageElement in dfuImage.ImageElements) {
+                dfuImageElement.Write(stream);
+            }
+        }
+    }
+}
