16daa7d28885caaae7d04cd1c2d032af52f8117a
Vladimir <riuson@gmail.com>
2020-09-22 18:38:42 +0500

    Add DfuImages

---
+++ Sources/DfusSeConvLib/Extensions/DfuImagesExtension.cs
@@ -0,0 +1,17 @@
+﻿using DfuSeConvLib.Parts;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace DfuSeConvLib.Extensions {
+    public static class DfuImagesExtension {
+        public static uint CalculateSize(this DfuImages dfuImages) =>
+            Convert.ToUInt32(dfuImages.Images.Sum(x => x.CalculateSize()));
+
+        public static void Write(this DfuImages dfuImages, Stream stream) {
+            foreach (var image in dfuImages.Images) {
+                image.Write(stream);
+            }
+        }
+    }
+}
---
+++ Sources/DfusSeConvLib/Parts/DfuImages.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DfuSeConvLib.Parts {
+    /// <summary>
+    /// The images section placed between the prefix and suffix as a body of the DFU file, contains
+    /// a list of DFU images indexed by the alternate setting.
+    /// </summary>
+    public class DfuImages {
+        public List<DfuImage> Images { get; } = new List<DfuImage>();
+    }
+}
