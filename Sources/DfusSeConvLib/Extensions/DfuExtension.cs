6d1acf26701a2acbc3086b96a733deb2a4bfc311
Vladimir <riuson@gmail.com>
2020-09-22 20:03:46 +0500

    Add Dfu

---
+++ Sources/DfusSeConvLib/Extensions/DfuExtension.cs
@@ -0,0 +1,15 @@
+﻿using DfuSeConvLib.Parts;
+using System.IO;
+
+namespace DfuSeConvLib.Extensions {
+    public static class DfuExtension {
+        public static uint CalculateSize(this Dfu dfu) =>
+            dfu.Prefix.CalculateSize() + dfu.Images.CalculateSize() + dfu.Suffix.CalculateSize();
+
+        public static void Write(this Dfu dfu, Stream stream) {
+            dfu.Prefix.Write(stream, dfu.Images);
+            dfu.Images.Write(stream);
+            dfu.Suffix.Write(stream);
+        }
+    }
+}
---
+++ Sources/DfusSeConvLib/Parts/Dfu.cs
@@ -0,0 +1,7 @@
+﻿namespace DfuSeConvLib.Parts {
+    public class Dfu {
+        public DfuPrefix Prefix { get; set; }
+        public DfuImages Images { get; set; }
+        public DfuSuffix Suffix { get; set; }
+    }
+}
