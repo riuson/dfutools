bd0188579d122dbf6a350ffd2464427a3e6c13af
Vladimir <riuson@gmail.com>
2020-09-25 21:39:00 +0500

    Add Autofac, test with IoC container

---
+++ Sources/DfusSeConvLib.Tests/Parts/DfuLogicModule.cs
@@ -0,0 +1,53 @@
+﻿using Autofac;
+using DfuSeConvLib.Parts;
+using DfuSeConvLib.Serialization;
+
+namespace DfuSeConvLib.Tests.Parts {
+    public class DfuLogicModule : Module {
+        protected override void Load(ContainerBuilder builder) {
+            builder.RegisterType<ImageElement>()
+                .AsImplementedInterfaces()
+                .AsSelf();
+            builder.RegisterType<TargetPrefix>()
+                .AsImplementedInterfaces()
+                .AsSelf();
+            builder.RegisterType<DfuImage>()
+                .AsImplementedInterfaces()
+                .AsSelf();
+            builder.RegisterType<DfuImages>()
+                .AsImplementedInterfaces()
+                .AsSelf();
+            builder.RegisterType<DfuPrefix>()
+                .AsImplementedInterfaces()
+                .AsSelf();
+            builder.RegisterType<DfuSuffix>()
+                .AsImplementedInterfaces()
+                .AsSelf();
+            builder.RegisterType<Dfu>()
+                .AsImplementedInterfaces()
+                .AsSelf();
+
+            builder.RegisterType<ImageElementSerializer>()
+                .AsImplementedInterfaces()
+                .AsSelf();
+            builder.RegisterType<TargetPrefixSerializer>()
+                .AsImplementedInterfaces()
+                .AsSelf();
+            builder.RegisterType<DfuImageSerializer>()
+                .AsImplementedInterfaces()
+                .AsSelf();
+            builder.RegisterType<DfuImagesSerializer>()
+                .AsImplementedInterfaces()
+                .AsSelf();
+            builder.RegisterType<DfuPrefixSerializer>()
+                .AsImplementedInterfaces()
+                .AsSelf();
+            builder.RegisterType<DfuSuffixSerializer>()
+                .AsImplementedInterfaces()
+                .AsSelf();
+            builder.RegisterType<DfuSerializer>()
+                .AsImplementedInterfaces()
+                .AsSelf();
+        }
+    }
+}
