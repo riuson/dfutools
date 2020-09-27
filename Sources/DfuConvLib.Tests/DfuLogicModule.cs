02e3669b0ad698d00376f2530a84be595cd861ed
Vladimir <riuson@gmail.com>
2020-09-27 22:57:09 +0500

    Register types in test library, less references to autofac

---
+++ Sources/DfuConvLib.Tests/DfuLogicModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using DfuConvLib.Parts;
+
+namespace DfuConvLib.Tests {
+    public class DfuLogicModule : Module {
+        protected override void Load(ContainerBuilder builder) {
+            var dfuConvLib = typeof(Dfu).Assembly;
+            builder.RegisterAssemblyTypes(dfuConvLib)
+                .AsImplementedInterfaces()
+                .AsSelf();
+        }
+    }
+}
