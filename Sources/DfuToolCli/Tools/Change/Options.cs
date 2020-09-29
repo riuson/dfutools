8b88d840e4c01147b564fb9d7e4ba6ac6e177f27
Vladimir <riuson@gmail.com>
2020-09-29 21:21:56 +0500

    Add command to change device, product id, vendor id in existing dfu file

---
+++ Sources/DfuToolCli/Tools/Change/Options.cs
@@ -0,0 +1,46 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using DfuToolCli.Interfaces;
+using System.Collections.Generic;
+
+namespace DfuToolCli.Tools.Change {
+    [Verb("change", HelpText = "Change IDs of DFU file.")]
+    internal class Options : IVerbOptions {
+        [Value(0)] public string File { get; set; }
+
+        [Option(
+            'd',
+            "device",
+            Required = false,
+            Default = "",
+            HelpText = "New firmware version.")]
+        public string Device { get; set; }
+
+        [Option(
+            'p',
+            "product",
+            Required = false,
+            Default = "",
+            HelpText = "New device's Product ID.")]
+        public string Product { get; set; }
+
+        [Option(
+            'v',
+            "vendor",
+            Required = false,
+            Default = "",
+            HelpText = "New device's VEndor ID.")]
+        public string Vendor { get; set; }
+
+        [Usage(ApplicationAlias = "dfutoolcli")]
+        public static IEnumerable<Example> Examples =>
+            new List<Example> {
+                new Example(
+                    "Change Product ID of DFU file",
+                    new Options {
+                        File = "sample.dfu",
+                        Product = "0x1234"
+                    })
+            };
+    }
+}
---
+++ Sources/DfuToolCli/Tools/Change/Processor.cs
@@ -0,0 +1,62 @@
+﻿using DfuConvLib.Interfaces;
+using DfuToolCli.Helpers;
+using DfuToolCli.Interfaces;
+using System;
+using System.IO;
+
+namespace DfuToolCli.Tools.Change {
+    internal class Processor : IVerbProcessor {
+        private readonly Func<IDfu> _createDfu;
+        private readonly Func<IDfuImages> _createDfuImages;
+        private readonly Func<IDfuPrefix> _createDfuPrefix;
+        private readonly Func<IDfuSerializer> _createDfuSerializer;
+        private readonly Func<IDfuSuffix> _createDfuSuffix;
+
+        public Processor(
+            Func<IDfuSerializer> createDfuSerializer,
+            Func<IDfu> createDfu,
+            Func<IDfuPrefix> createDfuPrefix,
+            Func<IDfuImages> createDfuImages,
+            Func<IDfuSuffix> createDfuSuffix) {
+            this._createDfuSerializer = createDfuSerializer;
+            this._createDfu = createDfu;
+            this._createDfuPrefix = createDfuPrefix;
+            this._createDfuImages = createDfuImages;
+            this._createDfuSuffix = createDfuSuffix;
+        }
+
+        public void Process(IVerbOptions obj) {
+            var options = obj as Options;
+
+            var device = options.Device == string.Empty ? -1 : options.Device.ToInt32(0, 0xffff);
+            var product = options.Product == string.Empty ? -1 : options.Product.ToInt32(0, 0xffff);
+            var vendor = options.Vendor == string.Empty ? -1 : options.Vendor.ToInt32(0, 0xffff);
+
+            var dfuSerializer = this._createDfuSerializer();
+
+            using (var stream = new FileStream(options.File, FileMode.Create, FileAccess.ReadWrite)) {
+                var dfu = this._createDfu();
+
+                dfu.Prefix = this._createDfuPrefix();
+
+                dfu.Images = this._createDfuImages();
+
+                dfu.Suffix = this._createDfuSuffix();
+
+                if (device >= 0) {
+                    dfu.Suffix.Device = device;
+                }
+
+                if (product >= 0) {
+                    dfu.Suffix.Product = product;
+                }
+
+                if (vendor >= 0) {
+                    dfu.Suffix.Vendor = vendor;
+                }
+
+                dfuSerializer.Write(stream, dfu);
+            }
+        }
+    }
+}
