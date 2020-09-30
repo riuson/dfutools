c2fc9149c026c60231a2e16144c6390965149e3f
Vladimir <riuson@gmail.com>
2020-09-30 19:10:23 +0500

    Add command to remove image elements from target

---
+++ Sources/DfuToolCli/Tools/Targets/Clear/Options.cs
@@ -0,0 +1,44 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using DfuToolCli.Interfaces;
+using System.Collections.Generic;
+
+namespace DfuToolCli.Tools.Targets.Clear {
+    [Verb("target-clear", HelpText = "Remove all image elements from target.")]
+    internal class Options : IVerbOptions {
+        [Value(0)] public string File { get; set; }
+
+        [Option(
+            "id",
+            SetName = "ById",
+            Required = false,
+            Default = "",
+            HelpText = "Alternate Setting (Target ID) to clear.")]
+        public string Id { get; set; }
+
+        [Option(
+            "index",
+            SetName = "ByIndex",
+            Required = false,
+            Default = "",
+            HelpText = "Alternate Setting's (Target ID) index to clear.")]
+        public string Index { get; set; }
+
+        [Usage(ApplicationAlias = "dfutoolcli")]
+        public static IEnumerable<Example> Examples =>
+            new List<Example> {
+                new Example(
+                    "Remove all image elements from target by ID",
+                    new Options {
+                        File = "sample.dfu",
+                        Id = "1"
+                    }),
+                new Example(
+                    "Remove all image elements from target by index",
+                    new Options {
+                        File = "sample.dfu",
+                        Index = "1"
+                    })
+            };
+    }
+}
---
+++ Sources/DfuToolCli/Tools/Targets/Clear/Processor.cs
@@ -0,0 +1,56 @@
+﻿using DfuConvLib.Interfaces;
+using DfuToolCli.Helpers;
+using DfuToolCli.Interfaces;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace DfuToolCli.Tools.Targets.Clear {
+    internal class Processor : IVerbProcessor {
+        private readonly Func<IDfuDeserializer> _createDfuDeserializer;
+        private readonly Func<IDfuSerializer> _createDfuSerializer;
+
+        public Processor(
+            Func<IDfuSerializer> createDfuSerializer,
+            Func<IDfuDeserializer> createDfuDeserializer) {
+            this._createDfuSerializer = createDfuSerializer;
+            this._createDfuDeserializer = createDfuDeserializer;
+        }
+
+        public void Process(IVerbOptions obj) {
+            var options = obj as Options;
+
+            var targetId = string.IsNullOrEmpty(options.Id) ? -1 : options.Id.ToInt32(0, 255);
+            var targetIndex = string.IsNullOrEmpty(options.Index) ? -1 : options.Index.ToInt32(0, 255);
+
+            var dfuSerializer = this._createDfuSerializer();
+            var dfuDeserializer = this._createDfuDeserializer();
+
+            using (var stream = new FileStream(options.File, FileMode.Open, FileAccess.ReadWrite)) {
+                var dfu = dfuDeserializer.Read(stream);
+
+                if (targetId >= 0) {
+                    var image = dfu.Images.Images.FirstOrDefault(x => x.Prefix.AlternateSetting == targetId);
+
+                    if (image != null) {
+                        image.ImageElements.Clear();
+                    } else {
+                        throw new ArgumentException($"Target with ID = {targetId} was not found!");
+                    }
+                } else if (targetIndex >= 0) {
+                    if (targetIndex < dfu.Images.Images.Count) {
+                        dfu.Images.Images[targetIndex].ImageElements.Clear();
+                    } else {
+                        throw new IndexOutOfRangeException(
+                            $"Target with index == {targetIndex} not found in list of size {dfu.Images.Images.Count}!");
+                    }
+                }
+
+                stream.Seek(0, SeekOrigin.Begin);
+                stream.SetLength(0);
+
+                dfuSerializer.Write(stream, dfu);
+            }
+        }
+    }
+}
