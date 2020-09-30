a62c43331fb86462c5701fdf5aeb148f3253059f
Vladimir <riuson@gmail.com>
2020-09-30 20:41:03 +0500

    Add command to change ID and Name of target

---
+++ Sources/DfuToolCli/Tools/Targets/Change/Options.cs
@@ -0,0 +1,67 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using DfuToolCli.Interfaces;
+using System.Collections.Generic;
+
+namespace DfuToolCli.Tools.Targets.Change {
+    [Verb("target-change", HelpText = "Change target's ID and name.")]
+    internal class Options : IVerbOptions {
+        [Value(0)] public string File { get; set; }
+
+        [Option(
+            "id",
+            SetName = "ById",
+            Required = true,
+            Default = "",
+            HelpText = "Alternate Setting (Target ID) to clear.")]
+        public string Id { get; set; }
+
+        [Option(
+            "index",
+            SetName = "ByIndex",
+            Required = true,
+            Default = "",
+            HelpText = "Alternate Setting's (Target ID) index to clear.")]
+        public string Index { get; set; }
+
+        [Option(
+            "set-name",
+            Required = false,
+            Default = null,
+            HelpText = "Target name (0...255 ASCII characters).")]
+        public string SetName { get; set; }
+
+        [Option(
+            "set-id",
+            Required = false,
+            Default = "",
+            HelpText = "Alternate Setting (Target ID).")]
+        public string SetId { get; set; }
+
+        [Usage(ApplicationAlias = "dfutoolcli")]
+        public static IEnumerable<Example> Examples =>
+            new List<Example> {
+                new Example(
+                    "Change name of target by ID",
+                    new Options {
+                        File = "sample.dfu",
+                        Id = "1",
+                        SetName = "newname"
+                    }),
+                new Example(
+                    "Change ID of target by index",
+                    new Options {
+                        File = "sample.dfu",
+                        Index = "1",
+                        SetId = "2"
+                    }),
+                new Example(
+                    "Change ID of target by ID",
+                    new Options {
+                        File = "sample.dfu",
+                        Id = "1",
+                        SetId = "2"
+                    })
+            };
+    }
+}
---
+++ Sources/DfuToolCli/Tools/Targets/Change/Processor.cs
@@ -0,0 +1,74 @@
+﻿using DfuConvLib.Interfaces;
+using DfuToolCli.Helpers;
+using DfuToolCli.Interfaces;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace DfuToolCli.Tools.Targets.Change {
+    internal class Processor : IVerbProcessor {
+        private readonly Func<IDfuDeserializer> _createDfuDeserializer;
+        private readonly Func<IDfuSerializer> _createDfuSerializer;
+
+        public Processor(
+            Func<IDfuSerializer> createDfuSerializer,
+            Func<IDfuDeserializer> createDfuDeserializer) {
+            this._createDfuSerializer = createDfuSerializer;
+            this._createDfuDeserializer = createDfuDeserializer;
+        }
+
+        public void Process(IVerbOptions obj) {
+            var options = obj as Options;
+
+            var targetId = string.IsNullOrEmpty(options.Id) ? -1 : options.Id.ToInt32(0, 255);
+            var targetIndex = string.IsNullOrEmpty(options.Index) ? -1 : options.Index.ToInt32(0, 255);
+
+            var setTargetId = string.IsNullOrEmpty(options.SetId) ? -1 : options.SetId.ToInt32(0, 255);
+
+            var dfuSerializer = this._createDfuSerializer();
+            var dfuDeserializer = this._createDfuDeserializer();
+
+            void updateIds(ITargetPrefix targetPrefix) {
+                if (setTargetId >= 0) {
+                    targetPrefix.AlternateSetting = setTargetId;
+                }
+
+                if (options.SetName != null) {
+                    if (options.SetName != string.Empty) {
+                        targetPrefix.TargetName = options.SetName;
+                        targetPrefix.TargetNamed = true;
+                    } else {
+                        targetPrefix.TargetName = string.Empty;
+                        targetPrefix.TargetNamed = false;
+                    }
+                }
+            }
+
+            using (var stream = new FileStream(options.File, FileMode.Open, FileAccess.ReadWrite)) {
+                var dfu = dfuDeserializer.Read(stream);
+
+                if (targetId >= 0) {
+                    var image = dfu.Images.Images.FirstOrDefault(x => x.Prefix.AlternateSetting == targetId);
+
+                    if (image != null) {
+                        updateIds(image.Prefix);
+                    } else {
+                        throw new ArgumentException($"Target with ID = {targetId} was not found!");
+                    }
+                } else if (targetIndex >= 0) {
+                    if (targetIndex < dfu.Images.Images.Count) {
+                        updateIds(dfu.Images.Images[targetIndex].Prefix);
+                    } else {
+                        throw new IndexOutOfRangeException(
+                            $"Target with index == {targetIndex} not found in list of size {dfu.Images.Images.Count}!");
+                    }
+                }
+
+                stream.Seek(0, SeekOrigin.Begin);
+                stream.SetLength(0);
+
+                dfuSerializer.Write(stream, dfu);
+            }
+        }
+    }
+}
