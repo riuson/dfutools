6ede57c847f71b693c6d78323beb66bbce923d9a
Vladimir <riuson@gmail.com>
2020-10-01 23:27:43 +0500

    Add test for target-clear

--- Sources/DfuToolCli/Tools/Targets/Clear/Processor.cs
+++ Sources/DfuToolCli/Tools/Targets/Clear/Processor.cs
@@ -20,37 +20,41 @@ namespace DfuToolCli.Tools.Targets.Clear {
         public void Process(IVerbOptions obj) {
             var options = obj as Options;
 
-            var targetId = string.IsNullOrEmpty(options.Id) ? -1 : options.Id.ToInt32(0, 255);
-            var targetIndex = string.IsNullOrEmpty(options.Index) ? -1 : options.Index.ToInt32(0, 255);
+            var id = string.IsNullOrEmpty(options.Id) ? -1 : options.Id.ToInt32(0, 255);
+            var index = string.IsNullOrEmpty(options.Index) ? -1 : options.Index.ToInt32(0, 255);
 
+            using (var stream = new FileStream(options.File, FileMode.Open, FileAccess.ReadWrite)) {
+                this.ProcessInternal(stream, id, index);
+            }
+        }
+
+        internal void ProcessInternal(Stream stream, int id, int index) {
             var dfuSerializer = this._createDfuSerializer();
             var dfuDeserializer = this._createDfuDeserializer();
 
-            using (var stream = new FileStream(options.File, FileMode.Open, FileAccess.ReadWrite)) {
-                var dfu = dfuDeserializer.Read(stream);
+            var dfu = dfuDeserializer.Read(stream);
 
-                if (targetId >= 0) {
-                    var image = dfu.Images.Images.FirstOrDefault(x => x.Prefix.TargetId == targetId);
+            if (id >= 0) {
+                var image = dfu.Images.Images.FirstOrDefault(x => x.Prefix.TargetId == id);
 
-                    if (image != null) {
-                        image.ImageElements.Clear();
-                    } else {
-                        throw new ArgumentException($"Target with ID = {targetId} was not found!");
-                    }
-                } else if (targetIndex >= 0) {
-                    if (targetIndex < dfu.Images.Images.Count) {
-                        dfu.Images.Images[targetIndex].ImageElements.Clear();
-                    } else {
-                        throw new IndexOutOfRangeException(
-                            $"Target with index == {targetIndex} not found in list of size {dfu.Images.Images.Count}!");
-                    }
+                if (image != null) {
+                    image.ImageElements.Clear();
+                } else {
+                    throw new ArgumentException($"Target with ID = {id} was not found!");
                 }
-
-                stream.Seek(0, SeekOrigin.Begin);
-                stream.SetLength(0);
-
-                dfuSerializer.Write(stream, dfu);
+            } else if (index >= 0) {
+                if (index < dfu.Images.Images.Count) {
+                    dfu.Images.Images[index].ImageElements.Clear();
+                } else {
+                    throw new IndexOutOfRangeException(
+                        $"Target with index == {index} not found in list of size {dfu.Images.Images.Count}!");
+                }
             }
+
+            stream.Seek(0, SeekOrigin.Begin);
+            stream.SetLength(0);
+
+            dfuSerializer.Write(stream, dfu);
         }
     }
 }
